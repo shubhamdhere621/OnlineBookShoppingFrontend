47b9f8b8e661e012fd8e47e03faa7e897e4f85f5
1050-3075\admin <oneillaloha@gmail.com>
2023-03-09 19:24:59 +0530

    signup frontend validators response msg field required

--- Angularapp/src/app/signup/signup.component.ts
+++ Angularapp/src/app/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, ValidatorFn, Validators,ValidationErrors } from '@angular/forms';
+import { AbstractControl, AbstractControlOptions, FormBuilder, FormGroup, ValidatorFn, Validators,ValidationErrors, FormControl } from '@angular/forms';
 import { repeat } from 'rxjs';
+import { User, UserType } from '../models/models';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
 responseMsg:string = '';
@@ -18,8 +19,8 @@ constructor(private fb : FormBuilder){
       lastName:fb.control('',[Validators.required]),
       email:fb.control('',[Validators.required,Validators.email]),
       password:fb.control('',[Validators.required]),
-      confirmPassword:fb.control(''),
-      userType:fb.control('customer'),
+      confirmPassword:fb.control(''), 
+      userType:fb.control('user'),
   },
   
   {
@@ -27,7 +28,70 @@ constructor(private fb : FormBuilder){
 
   }as AbstractControlOptions);
 }
+
+signup() {
+  let user : User = {
+    id : 0,
+    firstName : this.signupForm.get('firstName')?.value,
+    lastName : this.signupForm.get('lastName')?.value,
+    email : this.signupForm.get('email')?.value,
+    password : this.signupForm.get('password')?.value,
+    userType: UserType.USER
+
+
+
+  };
+  console.log(user)
 }
+
+getFirstNameErrors(){
+  if (this.FirstName.hasError('required')) return 'Please Enter FirstName!!!'
+  return ' '
+}
+
+getLastNameErrors(){
+  if (this.LastName.hasError('required')) return 'Please Enter LastName!!!'
+  return ' '
+}
+
+getEmailErrors(){
+  if (this.Email.hasError('required')) return 'Please Enter Email Id !!!'
+  if (this.Email.hasError('email')) return 'Please Enter Valid Email Id!!!'
+  return ' '
+}
+
+
+
+getPasswordErrors(){
+  if (this.Password.hasError('required')) return 'Please Enter the Password !!!'
+  
+  return ' '
+}
+//getters 
+get FirstName() : FormControl{
+  return this.signupForm.get('firstName') as FormControl;
+}
+
+get LastName() : FormControl{
+  return this.signupForm.get('lastName') as FormControl;
+}
+
+get Email() : FormControl{
+  return this.signupForm.get('email') as FormControl;
+}
+
+get Password() : FormControl{
+  return this.signupForm.get('password') as FormControl;
+}
+
+get ConfirmPassword() : FormControl{
+  return this.signupForm.get('confirmPassword') as FormControl;
+}
+
+
+}
+
+
 
 //validator for confirmPassword
 export const confirmPasswordValidator: ValidatorFn = (
@@ -46,3 +110,6 @@ export const confirmPasswordValidator: ValidatorFn = (
     return {confirmPassword:true};
   }
 };
+
+
+
